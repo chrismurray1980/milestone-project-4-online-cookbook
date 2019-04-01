50ae01c1b6ea0d771a6dbab2ce76df2cfdadda89
Chris Murray <murray_christopher_2010@yahoo.co.uk>
2019-04-01 07:19:03 +0000

    added main.js file to add options to dropdown menmus and hold js functions and added link to base.html file

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+var cuisine = new Array("American", "Italian", "Spanish", "Chinese", "French", "Japanese", "German","Thai", 
+                        "Mexican", "English", "Scottish", "Irish", "Brazilian" );
+
+var mealtime = new Array("Breakfast", "Lunch", "Dinner", "Snack");
+
+var course = new Array("Starter", "Main", "Dessert");
