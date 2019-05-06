d94f7c3014529b9840d11b06f9404e21e7658b9c
Chris Murray <murray_christopher_2010@yahoo.co.uk>
2019-05-06 14:25:23 +0000

    updated requirements.txt

--- installed_packages.py
+++ installed_packages.py
@@ -4,3 +4,4 @@ installed_packages = pip.get_installed_distributions()
 installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
      for i in installed_packages])
 print(installed_packages_list) 
+
