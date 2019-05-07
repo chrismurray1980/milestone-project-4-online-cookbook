2f050bc0c7e91824305a02941362d227bd82ef02
Chris Murray <murray_christopher_2010@yahoo.co.uk>
2019-05-07 10:50:11 +0000

    added spaces to equals signs in doc

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ Mapper.ensure_all_indexes()
 
 def database_config_setup(filename):
     """Configure application to use either mongodb or mongo-in-memory db"""
-    database_config = os.getenv("MONGO_URI",'mongodb://localhost') 
-    #if filename == "__main__" else 'mim://localhost/test'
+    database_config = os.getenv("MONGO_URI",'mongodb://localhost') if filename == "__main__" else 'mim://localhost/test'
     return database_config
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'onlineCookbook'
-app.config["MONGO_URI"]=database_config_setup(__name__)
-session = ThreadLocalODMSession(bind=create_datastore(app.config["MONGO_URI"] ) )
+app.config["MONGO_URI"] = database_config_setup(__name__)
+session = ThreadLocalODMSession(bind = create_datastore(app.config["MONGO_URI"] ) )
 
 recipes_collection = session.db.recipes
 recipes_collection.drop_index("$**_text")
@@ -34,25 +33,25 @@ def get_recipes():
     except:
         print("Error in accessing database documents")
      
-@app.route('/search_results', methods=['POST'])
+@app.route('/search_results', methods = ['POST'])
 def search_results():
     """Display recipes returned from db based on text input"""
     try:
         # search_content = '"{}"'.format(request.form.get('searchContent'))
-        search_content=search_text_formatting('searchContent')
-        recipes=recipes_collection.find({"$text": {"$search": search_content}}, 
+        search_content = search_text_formatting('searchContent')
+        recipes = recipes_collection.find({"$text": {"$search": search_content}}, 
                                         {'_txtscr': {'$meta': 'textScore'}}).sort([('_txtscr', {'$meta':'textScore'})])
-        return render_template("search_results.html", recipes=recipes)
+        return render_template("search_results.html", recipes = recipes)
     except:
             print("Error accessing database documents")
             
-@app.route('/advanced_search_results', methods=['POST'])
+@app.route('/advanced_search_results', methods = ['POST'])
 def advanced_search_results():
     """Return recipes from mongodb based on advanced search fields"""
     try:
-        advanced_search_list=advanced_search_query_formatting(field_list[6:])
-        recipes=recipes_collection.find({"$and": advanced_search_list}).sort([('recipeUpvotes', -1)]).limit( 10 )
-        return render_template("search_results.html", recipes=recipes)
+        advanced_search_list = advanced_search_query_formatting(field_list[6:])
+        recipes = recipes_collection.find({"$and": advanced_search_list}).sort([('recipeUpvotes', -1)]).limit( 10 )
+        return render_template("search_results.html", recipes = recipes)
     except:
         print("Error accessing database documents")
         
@@ -64,11 +63,11 @@ def add_recipe():
     except:
         print("Error, could not render add recipe view")
     
-@app.route('/insert_recipe', methods=['POST'])
+@app.route('/insert_recipe', methods = ['POST'])
 def insert_recipe():
     """Insert recipe to db and display index.html"""
     try:
-        input_fields=insert_update_db_format(field_list)
+        input_fields = insert_update_db_format(field_list)
         recipes_collection.insert_one(input_fields)
         return redirect(url_for('get_recipes'))
     except:
@@ -78,17 +77,17 @@ def insert_recipe():
 def edit_delete_recipe(recipe_id):
     """Open edit/delete page for specific document"""
     try:
-        recipe=recipes_collection.find_one({"_id": ObjectId(recipe_id)})
-        return render_template("edit_delete_recipe.html", recipe=recipe)
+        recipe = recipes_collection.find_one({"_id": ObjectId(recipe_id)})
+        return render_template("edit_delete_recipe.html", recipe = recipe)
     except:
         print("Error accessing database documents")
         
-@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+@app.route('/update_recipe/<recipe_id>', methods = ["POST"])
 def update_recipe(recipe_id):
     """Update specific document with form elements"""
     try:
-        update_fields=insert_update_db_format(field_list[1:])
-        recipes_collection.update_one( {'_id': ObjectId(recipe_id)}, {"$set": update_fields}, upsert=True)
+        update_fields = insert_update_db_format(field_list[1:])
+        recipes_collection.update_one( {'_id': ObjectId(recipe_id)}, {"$set": update_fields}, upsert = True)
         return redirect(url_for('get_recipes'))
     except:
         print("Error updating database document")
@@ -97,8 +96,8 @@ def update_recipe(recipe_id):
 def show_recipe(recipe_id):
     """Show recipe page of specific document"""
     try:
-        recipe=recipes_collection.find_one({"_id": ObjectId(recipe_id)})
-        return render_template("show_recipe.html", recipe=recipe)
+        recipe = recipes_collection.find_one({"_id": ObjectId(recipe_id)})
+        return render_template("show_recipe.html", recipe = recipe)
     except:
         print("Error accessing database documents")
 
@@ -116,8 +115,8 @@ def like_recipe(recipe_id):
     """Add upvote to document when button clicked"""
     try:
         recipes_collection.update( {'_id': ObjectId(recipe_id)}, { '$inc': { 'recipeUpvotes': 1} } )
-        recipe=recipes_collection.find_one({"_id": ObjectId(recipe_id)})
-        return render_template("show_recipe.html", recipe=recipe)
+        recipe = recipes_collection.find_one({"_id": ObjectId(recipe_id)})
+        return render_template("show_recipe.html", recipe = recipe)
     except:
         print("Error accessing database documents")
 
@@ -131,30 +130,30 @@ def favourites():
 
 #Assisting variables and functions
 
-field_list=['recipeUpvotes', 'recipeName', 'recipeAuthor', 'recipeIngredients', 'recipeInstructions',
+field_list = ['recipeUpvotes', 'recipeName', 'recipeAuthor', 'recipeIngredients', 'recipeInstructions',
             'recipeImageLink', 'recipeCuisine', 'recipeCountryOfOrigin', 'recipeMealTime', 'recipeServings',
             'recipeDifficulty', 'recipePreparationTime', 'recipeCookingTime', 'recipeAllergen', 'recipeDietary',
             'recipeMainIngredient']
             
 def search_text_formatting(search_text):
     """Correctly format string for text search of entire db"""
-    formatted_search_text='\"'+request.form.get(search_text)+'\"'
+    formatted_search_text = '\"'+request.form.get(search_text)+'\"'
     return formatted_search_text
        
 def advanced_search_query_formatting(list):
     """Obtain input values for select boxes and append to list for advanced search query"""
-    search_list=[]
+    search_list = []
     for value in list:
         if request.form.get(value) != '':
-            if value =='recipeAllergen' or value =='recipeDietary':
+            if value == 'recipeAllergen' or value == 'recipeDietary':
                 value_text = request.form.get(value)
                 value_split_text = value_text.split(', ')
-                search_subset=[]
+                search_subset = []
                 for i in value_split_text:
                     search_subset.append(i)
                 search_list.append({value : { '$in': search_subset} })  
-            elif value=='recipePreparationTime' or value=='recipeCookingTime' or value=='recipeServings':
-                if request.form.get(value).isdigit()==True:
+            elif value == 'recipePreparationTime' or value == 'recipeCookingTime' or value == 'recipeServings':
+                if request.form.get(value).isdigit() == True:
                     search_list.append({value: { '$lte': int(request.form.get(value))} })
             else:
                 search_list.append({value : request.form.get(value)})
@@ -162,20 +161,20 @@ def advanced_search_query_formatting(list):
        
 def insert_update_db_format(list):
     """Construct format of insert or update to be sent to db"""
-    field_input_dict={}
+    field_input_dict = {}
     for field in list:
-        if field =='recipeAllergen' or field =='recipeDietary':
-            field_value=request.form.get(field)
-            field_value=request.form.get(field) if field_value != '' else 'None'
-            field_input_dict[field] = field_value.split(',')
-        elif field=='recipePreparationTime' or field=='recipeCookingTime' or field=='recipeServings':
+        if field == 'recipeAllergen' or field == 'recipeDietary':
             field_value = request.form.get(field)
-            field_input_dict[field] = int(field_value) if field_value.isdigit()==True else 0
-        elif field =='recipeUpvotes' in list:
+            field_value = request.form.get(field) if field_value != '' else 'None'
+            field_input_dict[field] = field_value.split(',')
+        elif field == 'recipePreparationTime' or field == 'recipeCookingTime' or field == 'recipeServings':
+            field_value = request.form.get(field)
+            field_input_dict[field] = int(field_value) if field_value.isdigit() == True else 0
+        elif field == 'recipeUpvotes' in list:
             field_input_dict[field] = 0
         else:
             field_input_dict[field] = request.form.get(field)
     return field_input_dict         
      
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
+if __name__  ==  '__main__':
+    app.run(host = os.environ.get('IP'), port = int(os.environ.get('PORT')), debug = True)
